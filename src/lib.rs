7857043c7001e7c5f6cdc8473ee1e1ce901e1137
Jay Jackson <jj@jj.is-a.dev>
2022-03-25 23:29:29 -0400

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,23 @@ use serenity::{
     prelude::*,
     Result as SResult,
 };
-use std::{any::Any, collections::BTreeMap};
+use std::{any::Any, collections::BTreeMap, fmt::Debug};
 
-pub struct Storage<T: Ord> {
+pub struct Storage<T: Ord + Debug> {
     pub guild: Guild,
     pub data: BTreeMap<T, Box<dyn Any + Send + Sync>>,
     pub channel: GuildChannel,
     pub ctx: Box<Context>,
+}
+
+impl<T: Ord + Debug> Debug for Storage<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Storage {{ guild: {:?}, data: {:?}, channel: {:?} }}",
+            self.guild, self.data, self.channel
+        )
+    }
 }
 
 pub enum GetError {
@@ -20,11 +30,14 @@ pub enum GetError {
     WrongType,
 }
 
-impl<T: Ord> Storage<T> {
+impl<T: Ord + Debug> Storage<T> {
     pub async fn new(mut guild: Guild, ctx: Box<Context>) -> SResult<Self> {
-        let channel = guild.channels.values_mut().find(|c| match c {
-            Channel::Guild(c) => c.name == "storage",
-            _ => unreachable!(),
+        let channel = guild.channels.values_mut().find(|c| {
+            if let Channel::Guild(c) = c {
+                c.name == "storage"
+            } else {
+                false
+            }
         });
 
         let channel = match channel {
@@ -35,7 +48,9 @@ impl<T: Ord> Storage<T> {
             None => {
                 guild
                     .create_channel(&ctx.http, |c| {
-                        c.name("storage").kind(ChannelType::Text).position(u32::MAX)
+                        c.name("storage")
+                            .kind(ChannelType::Text)
+                            .position(i32::MAX as u32)
                     })
                     .await?
             }
